f20f89d718d2185c32e97354567ea90d50373608
Fabricio Brandan <brandanfabricio7@gmai.com>
2022-12-13 12:41:44 -0300

    arreglo paginas

--- src/database/index.js
+++ src/database/index.js
@@ -19,7 +19,7 @@ const Sequelize = require('sequelize')
 const db = new Sequelize('misnovelas', 'root', 'root', {
 
     port: '3007',
-    host: 'Mysql',
+    host: '192.168.1.23',
     dialect: 'mysql'
 })
 
